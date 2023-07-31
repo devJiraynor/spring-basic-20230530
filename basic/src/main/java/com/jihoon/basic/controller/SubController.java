03cff36b72d0695d31ec7a2e200807de507f0948
devJiraynor <jiraynor@naver.com>
2023-07-31 18:17:36 +0900

    2023.07.31. 18:20

---
+++ basic/src/main/java/com/jihoon/basic/controller/SubController.java
@@ -0,0 +1,17 @@
+package com.jihoon.basic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// description: http://localhost:8080/sub //
+@RequestMapping("/sub")
+public class SubController {
+  
+  @GetMapping("/")
+  public String getMethod() {
+    return "sub get method";
+  }
+
+}
